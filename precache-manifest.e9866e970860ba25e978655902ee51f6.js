509d5c53e23622c9a159dd8097cef0c22d3a1a28
Ari Nurcahya <nurcahyaari@gmail.com>
2019-11-10 12:27:24 +0700

    Updates

---
+++ precache-manifest.e9866e970860ba25e978655902ee51f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "579bc988fbf74ecc8ca3134c056b149a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "860721602332112a21fd",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d40a7fe1feec7405aeb2",
+    "url": "/static/js/2.94e3a532.chunk.js"
+  },
+  {
+    "revision": "860721602332112a21fd",
+    "url": "/static/js/main.697330dc.chunk.js"
+  },
+  {
+    "revision": "742f1f7fc548cb268f56",
+    "url": "/static/js/runtime-main.3d4f50df.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
